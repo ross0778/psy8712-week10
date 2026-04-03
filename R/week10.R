364b98523eb14ccb37e198f86b661ea32ea55f3b
ross0778 <ross0778@umn.edu>
2026-04-03 09:49:31 -0500

    Keeps haven_labelled vectors, factor() was messing up visualization

--- R/week10.R
+++ R/week10.R
@@ -8,8 +8,6 @@ library(haven)
 # zap_missing() converts tagged missing values into standard R NA value. Used read_sav() from haven to read in GSS2016.sav
 gss_tbl <- read_sav("../data/GSS2016.sav", user_na = TRUE) %>% 
   zap_missing() %>% 
-  # this converts haven_labelled vectors to factors
-  mutate(across(where(is.labelled), as_factor)) %>% 
   # using filter() here removes anyone who has a missing value for work hours
   filter(!is.na(mosthrs)) %>% 
   # using select() here removes the other variables referencing hours per week
@@ -17,4 +15,4 @@ gss_tbl <- read_sav("../data/GSS2016.sav", user_na = TRUE) %>%
   # using select_if() to only select columns with less than 75% missingness. The mean shows the proportion of NA values in the given column
   select_if(~ mean(is.na(.)) < 0.75)
   
-
+# Visualization
